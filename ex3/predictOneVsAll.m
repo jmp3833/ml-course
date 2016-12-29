0dac4ff99a96ed42d2cea40404da747ae017300d
jpeterson <jpeterson@hubspot.com>
2016-12-29 10:54:37 -0500

    Update ex3 template

--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -1,8 +1,12 @@
 function p = predictOneVsAll(all_theta, X)
-%PREDICT Predict whether the label is 0 or 1 using learned logistic 
-%regression parameters all_theta
-%   p = PREDICT(all_theta, X) computes the predictions for X using a 
-%   threshold at 0.5 (i.e., if sigmoid(all_theta'*x) >= 0.5, predict 1)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1). 
+%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+%  for each example in the matrix X. Note that X contains the examples in
+%  rows. all_theta is a matrix where the i-th row is a trained logistic
+%  regression theta vector for the i-th class. You should set p to a vector
+%  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
+%  for 4 examples) 
 
 m = size(X, 1);
 num_labels = size(all_theta, 1);
@@ -25,6 +29,12 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
+
+
+
+
+
+
 
 % =========================================================================
 
