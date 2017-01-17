1cf1efb93da802c8f05a37a5787724b47c96e0e1
Justin Peterson <jmp3833@rit.edu>
2017-01-16 21:43:36 -0500

    Add parameter calculation function

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,12 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+valuesToTry = [0.01 0.03 0.1 0.3 1 3 10 30];
+lowest_predicted_error = inf;
 
+for i = 1:8
+  for j = 1:8
+    % create model with varied values of C and Sigma
+    C_to_try = valuesToTry(i);
+    sigma_to_try = valuesToTry(j);
+    model = svmTrain(X, y, C_to_try, @(x1, x2) gaussianKernel(x1, x2, sigma_to_try)); 
 
+    %Validate local model against cross validation set
+    predictions = svmPredict(model, Xval);
+    pred_error = mean(double(predictions ~= yval));
 
-
-
-
+    %If predicted error with C and sigma is lower than lowest
+    %currently found, replace global C and sigma
+    if pred_error < lowest_predicted_error
+      C = C_to_try;
+      sigma = sigma_to_try;
+      lowest_predicted_error = pred_error;
+    end
+  end
+end
 % =========================================================================
-
 end
